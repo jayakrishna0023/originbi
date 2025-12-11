f76d272778f0ee3cb077d69aa80bcf4c3df83784
nayira <114664058+vd8761@users.noreply.github.com>
2025-12-11 13:31:09 +0530

    Update Auth Services

---
+++ backend/auth-service/dist/cognito/cognito.config.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.cognitoConfig = void 0;
+// backend/auth-service/src/cognito/cognito.config.ts
+exports.cognitoConfig = {
+    userPoolId: 'ap-south-1_ysb2qFXrb', // <-- fill from AWS Cognito
+    clientId: '5irno605169eg55eq3abouj91i', // <-- your app client ID
+    region: 'ap-south-1',
+};
+//# sourceMappingURL=cognito.config.js.map
